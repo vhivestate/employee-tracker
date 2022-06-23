491191d8cdd812ae26a5140bb22cc503834aea09
vhivestate <garcia.valeria001@gmail.com>
2022-06-23 14:49:23 -0600

    created tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -11,7 +17,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+USE employee_db;
+
+INSERT INTO departments (name)
+VALUES
+('Garden'),
+('Furniture'),
+('Tools');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Sales', 32.000, 2),
+  ('Cashier', 17.000, 1),
+  ('Returns', 22.000, 2),
+  ('Management', 60.000, 3);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Virginia', 'Woolf', 4, NULL),
+  ('Piers', 'Gaveston', 2, 1),
+  ('Charles', 'LeRoi', 2, 1),
+  ('Katherine', 'Mansfield', 4, NULL),
+  ('Dora', 'Carrington', 3, 4),
+  ('Edward', 'Bellamy', 3, 4),
+  ('Montague', 'Summers', 4, NULL),
+  ('Octavia', 'Butler', 1, 7),
+  ('Unica', 'Zurn', 1, 7);
