6612d47a471a44fa10ddddec02482214cc5db170
vhivestate <garcia.valeria001@gmail.com>
2022-06-23 21:27:18 -0600

    added connection file

--- app.js
+++ app.js
@@ -1,30 +1,5 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'P8RX77z7',
-        database: 'employee_db'
-    });
+require("dotenv").config();
+const { Console } = require("console");
+const inquirer = require("inquirer");
+const db = require("./db/connection");
 
-
-function userInput() {
-    inquirer.prompt([{
-        type: "list",
-        name: "role",
-        message: "What would you like to do?",
-        choices: [
-            "View All Roles",
-            "Add Role",
-            "View All Employees",
-            "Add Employee",
-            "Update Employee Role",
-            "View All Departments",
-            "Add Department"
-        ],
-    }])
-    console.log(userInput);
-}
-
-userInput();
---
+++ db/connection.js
@@ -0,0 +1,14 @@
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+  },
+  console.log("Database Connected")
+);
+
+module.exports = db;
