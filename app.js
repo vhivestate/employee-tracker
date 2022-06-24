6e3e13d05fb3dcf878e0508b0bf37de6e3a89ba4
vhivestate <garcia.valeria001@gmail.com>
2022-06-23 22:38:44 -0600

    added fuunction

--- app.js
+++ app.js
@@ -3,3 +3,40 @@ const { Console } = require("console");
 const inquirer = require("inquirer");
 const db = require("./db/connection");
 
+initialize = () => {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Employee', 'Add Role', 'Update Employee Role', 'Quit']
+        }).then((data) => {
+            switch (data.choices) {
+                case 'View Departments':
+                    viewDep();
+                    break;
+                case 'View Roles':
+                    viewRoles();
+                    break;
+                case 'View Employees':
+                    viewAllEmp();
+                    break;
+                case 'Add Department':
+                    addDep();
+                    break;
+                case 'Add Employee':
+                    addEmp();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'Update Employee Role':
+                    upEmp();
+                    break;
+                case 'Quit':
+                    break;
+            }
+            console.log(data.choices + ' chosen!!!');
+        })
+};
+initialize();
